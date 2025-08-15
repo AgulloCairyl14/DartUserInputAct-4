a87268acfb74dd015308733abea8e2c2e1616bc0
Cairyl Ella Agullo <168385988+AgulloCairyl14@users.noreply.github.com>
2025-08-15 17:06:46 +0800

    Dart User Input Computation

---
+++ Act4.dart
@@ -0,0 +1,7 @@
+name = input("Enter your Name: ")
+section = input("Enter your Section: ")
+subject = input("Enter your Subject: ")
+
+num1 = float(input("Enter Num1: "))
+num2 = float(input("Enter Num2: "))
+num3 = float(input("Enter Num3: "))
